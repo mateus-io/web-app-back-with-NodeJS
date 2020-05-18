3128767044078d9fffb4ffada9c65b35da089221
Computrailer <letsgood56@gmail.com>
2020-05-17 23:46:00 -0300

    repair cors issues and putting dotenv dependency

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,10 @@ const authRoutes = require('./routes/auth-routes');
 const profileRoutes = require('./routes/profile-routes');
 const mongoose = require('mongoose');
 const passportSetup = require('../config/passport-setup');
-const keys = require('../config/keys');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = new express();
 
@@ -14,21 +15,22 @@ app.set('view engine', 'ejs');
 
 app.use(cookieSession({
     maxAge : 24 * 60 * 60 * 1000,
-    keys: [keys.session.cookieKey],
+    keys: '[keys.session.cookieKey]',
 }));
 
 //initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect(keys.mongodb.key, {
+mongoose.connect(process.env.MONGO_DB_KEY, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
 
-app.listen(3000);
+app.listen(8383);
